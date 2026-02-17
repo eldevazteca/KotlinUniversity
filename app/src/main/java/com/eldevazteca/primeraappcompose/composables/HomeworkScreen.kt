1ecc87b55172cbe3da1cb35d8344c2a20e43010f
eldevazteca <eldevazteca@gmail.com>
2026-02-17 15:06:23 -0600

    Vistas completadas

--- app/src/main/java/com/eldevazteca/primeraappcompose/composables/HomeworkScreen.kt
+++ app/src/main/java/com/eldevazteca/primeraappcompose/composables/HomeworkScreen.kt
@@ -12,8 +12,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -43,8 +46,9 @@ fun HomeWorkScreen(modifier: Modifier) {
             modifier = Modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(12.dp))
+                .padding(top = 30.dp)
                 .background(Color.DarkGray)
-                .padding(8.dp),
+                .padding(vertical = 20.dp),
             color = Color.White
         )
         Spacer(modifier = Modifier.height(20.dp))
@@ -55,13 +59,15 @@ fun HomeWorkScreen(modifier: Modifier) {
                 .height(50.dp),
             horizontalArrangement = Arrangement.SpaceAround
         ) {
-            Button(onClick = {},
+            Button(
+                onClick = {},
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Black,
                     contentColor = Color.White
                 ),
                 modifier = Modifier
                     .height(60.dp)) {
+                Icon(Icons.Default.Check, contentDescription = null)
                 Text("Therian")
             }
             Button(onClick = {},
@@ -71,6 +77,7 @@ fun HomeWorkScreen(modifier: Modifier) {
                 ),
                 modifier = Modifier
                     .height(60.dp)) {
+                Icon(Icons.Default.Check, contentDescription = null)
                 Text("Furro")
             }
         }
@@ -101,10 +108,13 @@ fun HomeWorkScreen(modifier: Modifier) {
                 .fillMaxWidth()
         ) {
             Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF0fA0Fa),
+                    contentColor = Color.White
+                ),
                 onClick = {}, modifier = Modifier
                     .padding(horizontal = 50.dp)
                     .fillMaxWidth()
-                    .height(60.dp)
             ) {
                 Text("Hola")
             }
